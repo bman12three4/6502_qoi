47cd3f1b1a1dbe30032eac1bbc5853a5496b1dc3
Byron Lathi <bslathi19@gmail.com>
2023-05-20 22:30:01 -0500

    Testing code for accelerator interface

--- sw/main.c
+++ sw/main.c
@@ -3,6 +3,7 @@
 
 char* qoi = (char*)0x9000;
 char* img = (char*)0x8000;
+char* accel = (char*)0xa000;
 
 qoi_desc desc;
 
@@ -13,6 +14,7 @@ int main(void)
 	desc.height = 32;
 	desc.channels = 4;
 	desc.colorspace = QOI_SRGB;
-	s_qoi_encode(img, &desc, &size);
+	//s_qoi_encode(img, &desc, &size);
+	accel[1] = 1;
 	return 0;
 }
